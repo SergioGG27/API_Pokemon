f15424ae9cc1378b3e5fe97f8c4d7e565dc67aa5
SergioGG27 <sagomezg55@gmail.com>
2022-08-18 20:07:32 -0500

    ya terminado

--- assets/code.js
+++ assets/code.js
@@ -3,16 +3,28 @@ let consumoApi = fetch(api_pokemon_nombreUrl)
 consumoApi.then(nomAndUrl => nomAndUrl.json())
         .then((nombre_url) => {
             for (const nomPersonaje of nombre_url.results) {
-                let consumoApi2 = fetch(nombre_url.url)
-                consumoApi2.then(abilidadesAndImg => abilidadesAndImg.json)
+                let consumoApi2 = fetch(nomPersonaje.url)
+                consumoApi2.then(abilidadesAndImg => abilidadesAndImg.json())
                 .then((abilidadImg) => {
-                    for (const cualidadImg of abilidadImg.abilities) {
-                        
-                        for (const cualidadImg of abilidadImg.forms){
-
+                    let cualidad = ''
+                    for (const cualidadImg1 of abilidadImg.abilities) {                       
+                        cualidad += cualidadImg1.ability.name
                         }
-                    }
+
+                document.querySelector('#cartas-personajes').innerHTML += `
+                <div class="col">
+                <div class="card">
+                    <img src="${abilidadImg.sprites.other.dream_world.front_default}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${abilidadImg.name}</h5>
+                        <p class="card-text">${cualidad}</p>
+                    </div>
+                </div>
+            </div>
+
+                `
+                    
                 }
-                )
+                )  
             }
         })
